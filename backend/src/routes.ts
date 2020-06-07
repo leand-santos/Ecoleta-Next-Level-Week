0ea57cd67799a69bd39576e0fd5c190795443bd8
leand-santos <leandro.adriano428@gmail.com>
2020-06-07 16:57:08 -0300

    set frontend finish backend

---
+++ backend/src/routes.ts
@@ -0,0 +1,17 @@
+import express, { response } from 'express'
+
+import ItemsController from './controllers/ItemsController'
+import PointsController from './controllers/PointsController'
+// index, show, create, update, delete
+
+const routes = express.Router()
+const pointsController = new PointsController()
+const itemsController = new ItemsController()
+
+routes.get('/items', itemsController.index)
+
+routes.post('/points', pointsController.create)
+routes.get('/points', pointsController.index)
+routes.get('/points/:id', pointsController.show)
+
+export default routes
